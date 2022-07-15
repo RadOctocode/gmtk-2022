fc75f0ecd4f337c5e268a2fbb088035e4f0eec19
Michelle Tan <mtan2198@gmail.com>
2022-07-15 18:35:00 -0400

    adding click pathfinding

--- GMTK game2022/Assets/Scripts/clickToMove.cs
+++ GMTK game2022/Assets/Scripts/clickToMove.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class clickToMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public NavMeshAgent agent;
+    public Interactable clicked;
 
     // Update is called once per frame
+    public void setObject(Interactable objective) {
+        clicked = objective;
+    }
+
+    void Start() {
+        clicked = null;
+    }
+
     void Update()
     {
-        
+        if(/*Input.GetMouseButtonDown(1)*/clicked != null){
+            Ray movePosition = Camera.main.ScreenPointToRay(clicked.transform.position);
+            if(Physics.Raycast(movePosition, out var hitInfo)){
+                agent.SetDestination(hitInfo.point);
+                Debug.Log(hitInfo.point);
+            }
+        }
     }
+
 }
